86ba7d606e46b3b493a7884989cdda11fe79735c
paxal <cpascal@igraal.com>
2014-04-14 11:00:20 +0200

    Add ParamterBag tests

---
+++ tests/Tests/Tools/ParameterBagTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Tools;
+
+use IgraalOSL\StatsTable\Tools\ParameterBag;
+
+class ParameterBagTest extends \PHPUnit_Framework_TestCase
+{
+    private function getSampleData()
+    {
+        return array(
+            '1' => 'One',
+            '2' => 'Two',
+        );
+    }
+
+    private function getSampleBag()
+    {
+        return new ParameterBag($this->getSampleData());
+    }
+
+    public function testConstructorWithArray()
+    {
+        $data = $this->getSampleData();
+        $bag = $this->getSampleBag();
+
+        $this->assertEquals($data, $bag->toArray());
+    }
+
+    public function testConstructorWithParameterBag()
+    {
+        $data = $this->getSampleData();
+        $bag = $this->getSampleBag();
+
+        $bag2 = new ParameterBag($bag);
+        $this->assertEquals($data, $bag2->toArray());
+    }
+
+    public function testHasKey()
+    {
+        $bag = $this->getSampleBag();
+
+        $this->assertTrue($bag->has('1'));
+        $this->assertFalse($bag->has('3'));
+    }
+
+    public function testGet()
+    {
+        $bag = $this->getSampleBag();
+
+        $this->assertEquals('One', $bag->get('1', 'One value'));
+        $this->assertEquals('Three', $bag->get('3', 'Three'));
+    }
+}
