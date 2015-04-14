6845222d383387e53a60045508c1e3d5f9c83747
paxal <cpascal@igraal.com>
2015-04-14 18:45:59 +0200

    Fix 5.3 version

--- tests/Tests/DynamicColumn/RelativeColumnBuilderTest.php
+++ tests/Tests/DynamicColumn/RelativeColumnBuilderTest.php
@@ -10,27 +10,27 @@ class RelativeColumnBuilderTest extends \PHPUnit_Framework_TestCase
     public function testWithData()
     {
         $statsTable = new StatsTableBuilder(
-            [
-                'first' => ['a' => 1, 'b' => 2, 'c' => 0],
-                'second' => ['a' => 4, 'b' => 5, 'd' => 0]
-            ]
+            array(
+                'first' => array('a' => 1, 'b' => 2, 'c' => 0),
+                'second' => array('a' => 4, 'b' => 5, 'd' => 0)
+            )
         );
 
         $aColumnBuilder = new RelativeColumnBuilder('a');
         $this->assertEquals(
-            ['first' => .2, 'second' => .8],
+            array('first' => .2, 'second' => .8),
             $aColumnBuilder->buildColumnValues($statsTable)
         );
 
-        $abColumnBuilder = new RelativeColumnBuilder(['a', 'b']);
+        $abColumnBuilder = new RelativeColumnBuilder(array('a', 'b'));
         $this->assertEquals(
-            ['first' => .25, 'second' => .75],
+            array('first' => .25, 'second' => .75),
             $abColumnBuilder->buildColumnValues($statsTable)
         );
 
-        $cColumnBuilder = new RelativeColumnBuilder(['c']);
+        $cColumnBuilder = new RelativeColumnBuilder('c');
         $this->assertEquals(
-            ['first' => 0, 'second' => 0],
+            array('first' => 0, 'second' => 0),
             $cColumnBuilder->buildColumnValues($statsTable)
         );
     }
